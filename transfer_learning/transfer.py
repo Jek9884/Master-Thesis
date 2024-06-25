ee7fab07871787ff19df93c55e2d118b3f4afd6d
Alessandro <a.capurso1@studenti.unipi.it>
2024-06-25 19:15:09 +0200

    add script parameters and fix bugs

--- transfer_learning/transfer.py
+++ transfer_learning/transfer.py
@@ -1,14 +1,14 @@
 import torch
 import os
-import gymnasium as gym
 import sys
 sys.path.insert(1, "../")
-from stable_baselines3.sac.sac import SAC, SACMaster
+from stable_baselines3.sac.sac import SACMaster
 from stable_baselines3.common.env_util import make_vec_env
 from ae.imageAE.highway_model import HighwayEnvModel
 import time
 from stable_baselines3.common.callbacks import BaseCallback
 from tqdm import tqdm
+import argparse
 
 class ProgressBarCallback(BaseCallback):
     def __init__(self, total_timesteps, verbose=0):
@@ -26,21 +26,47 @@ class ProgressBarCallback(BaseCallback):
     def _on_training_end(self) -> None:
         self.pbar.close()
 
-env_name = "racetrack-v0"
+parser = argparse.ArgumentParser(description="Train a RL agent using TL")
 
-# Without transfer learning
-env = make_vec_env(env_name, n_envs=32)
+# Define command-line arguments
+parser.add_argument("--env_name", type=str, help="Environment to use")
+parser.add_argument("--policy_dir", type=str, help="Path to the source policies")
+parser.add_argument("--experience_dir", type=str, help="Path to the source dataset")
+parser.add_argument("--description_dir", type=str, help="Path to the source tasks descriptions")
+parser.add_argument("--ae_path", type=str, help="Path to the ae model")
+parser.add_argument("--device", type=str, help="Device where to execute")
+parser.add_argument("--transfer_type", type=int, help="0 for no transfer CV or 1 for transfer")
+parser.add_argument("--total_timesteps", type=int, help="Max number of timesteps to perform")
+parser.add_argument("--n_envs", type=int, default=1, help="Number of environments to use in parallel")
+parser.add_argument("--seed", type=int, default=42, help="Seed to use for replicability")
+parser.add_argument("--k", type=int, help="Number of time-steps to perform before to use the metric")
+parser.add_argument("--similarity_thr", type=float, help="Threshold for the metric")
 
-# With transfer learning
-#env = gym.make(env_name, render_mode='rgb_array')
+# Parse the command-line arguments
+args = parser.parse_args()
 
-seed = 42
-device = "2"
-policy_dir = "../highway-env/subpolicies"
-experience_dir = "./transfer_dataset"
-description_dir = "./descriptions.json"  
-ae_path = "../highway-env/final_model/best_highway_batch128_wd1e-7_lr1e-5_holdout.ckpt"
-#task_description = "boh"
+assert args.transfer_type == 0 or args.transfer_type == 1
+assert args.n_envs > 0
+assert args.similarity_thr >= -1 and args.similarity_thr <= 1
+assert args.k > 0
+
+env_name = args.env_name#"racetrack-v0"
+policy_dir = args.policy_dir
+experience_dir = args.experience_dir
+description_dir = args.description_dir
+ae_path = args.ae_path
+device = args.device
+transfer_type = args.transfer_type
+total_timesteps = args.total_timesteps
+n_envs = args.n_envs
+seed = args.seed
+k = args.k
+similarity_thr = args.similarity_thr
+
+task_description = None
+
+# Init env
+env = make_vec_env(env_name, n_envs=n_envs)
 
 if device != "cpu":
     os.environ["CUDA_VISIBLE_DEVICES"] = device
@@ -48,29 +74,28 @@ if device != "cpu":
 
 ae = HighwayEnvModel.load_from_checkpoint(ae_path)
 
-total_timesteps = 1e7
 progress_bar_callback = ProgressBarCallback(total_timesteps)
 
-# Without transfer learning
-agent = SACMaster(env=env, policy='MlpPolicy', learning_rate=1e-4, batch_size=1024, tau=0.9, gamma=0.99, gradient_steps=10, 
-            use_sde=False, seed=seed)
+if transfer_type == 0:
+    # Without transfer learning
+    agent = SACMaster(env=env, policy='MlpPolicy', learning_rate=1e-4, batch_size=1024, tau=0.9, gamma=0.99, gradient_steps=10, 
+                        use_sde=False, device=device, seed=seed)
+else:
+    if task_description is not None:
+        # TL with task description
+        agent = SACMaster(env=env, policy='MlpPolicy', learning_rate=1e-4, batch_size=1024, tau=0.9, gamma=0.99, gradient_steps=10, 
+                            use_sde=False, policy_dir=policy_dir, experience_dir=experience_dir, 
+                            descriptions_dir=description_dir, ae_model=ae, k=k, similarity_thr=similarity_thr, task_description=task_description,
+                            tokenizer_str="bert-base-cased", device=device, seed=seed)
+    else:
+        # TL without task description
+        agent = SACMaster(env=env, policy='MlpPolicy', learning_rate=1e-4, batch_size=1024, tau=0.9, gamma=0.99, gradient_steps=10, 
+                            use_sde=False, policy_dir=policy_dir, experience_dir=experience_dir, 
+                            descriptions_dir=description_dir, ae_model=ae, k=k, similarity_thr=similarity_thr, device=device, seed=seed)
 
-"""# With task description
-agent = SACMaster(env=env, policy='MlpPolicy', learning_rate=1e-4, batch_size=1024, tau=0.9, gamma=0.99, gradient_steps=10, 
-                    use_sde=False, policy_dir=policy_dir, experience_dir=experience_dir, 
-                    descriptions_dir=description_dir, ae_model=ae, k=20, similarity_thr=0.6, task_description=task_description,
-                    tokenizer_str="bert-base-cased")"""
-
-
-"""# Without task description
-agent = SACMaster(env=env, policy='MlpPolicy', learning_rate=1e-4, batch_size=1024, tau=0.9, gamma=0.99, gradient_steps=10, 
-                    use_sde=False, policy_dir=policy_dir, experience_dir=experience_dir, descriptions_dir=description_dir, 
-                    ae_model=ae, k=20, similarity_thr=0.6, device=device)"""
-
-start_time = time.time()
 agent.learn(total_timesteps=total_timesteps, callback=progress_bar_callback)
-end_time = time.time()
-elapsed_time = end_time - start_time
-print("Elapsed time: ", elapsed_time)
 
-agent.save("racetrack_no_transfer_16env")
+if transfer_type == 1:
+    agent.save(f"{env_name}_{transfer_type}_{n_envs}env_{k}_{similarity_thr}")
+else:
+    agent.save(f"{env_name}_{transfer_type}_{n_envs}env")
